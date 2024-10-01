4bf05b301a696865b1a27b5d669c77acafccafa9
EZEQUIEL FELICIANO DOS SANTOS <ezequiel.fsantos10@senacsp.edu.br>
2024-10-01 17:18:46 -0300

    first commit

---
+++ Exercicio 5/script.js
@@ -0,0 +1,13 @@
+//let contador = 1;
+//const numero = 5
+
+// while (contador <=10) {
+//     console.log(`${numero} x ${contador } = ${numero * contador}`);
+//     contador++;
+// }
+
+const numero = 5;
+
+for(let contador = 1; contador <=10; contador++){
+    console.log(`${numero} x ${contador} = ${numero * contador}`);
+}
